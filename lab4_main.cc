683d8244d2c522120393490315c2014accb4e166
tmodi001 <tmodi001@ucr.edu>
2015-05-04 15:32:59 -0700

    Rename main.cc to lab4_main.cc

---
+++ lab4_main.cc
@@ -0,0 +1,15 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+// Name: Tirth Modi
+// SID: 861168370
+// Date: 05-04-2015
+//  ================== END ASSESSMENT HEADER ===============
+#include "lab4.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    printpreorder(atoi(argv[1]));
+    printpostorder(atoi(argv[1]));
+    printsorted(atoi(argv[1]));
+    return 0;
+}
